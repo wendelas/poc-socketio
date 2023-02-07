d655c622672feec9cb278d4016406bebc3441994
Wendel de Assis da Silva <wendelas@gmail.com>
2023-02-07 10:41:35 -0300

    update gitignore

---
+++ server.js
@@ -0,0 +1,22 @@
+const { Console } = require('console');
+const app = require('express')();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http)
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/index.html')
+})
+
+io.on('connection', (socket)=>{
+    console.log('new connection', socket.id);
+    socket.on('msg', (msg)=>{
+        console.log(msg);
+        socket.broadcast.emit('msg', msg);
+    })
+})
+
+http.listen(3000, function(){
+    console.log('listening on port 3000')
+
+});
